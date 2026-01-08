0654f7502fdccf763228dc296b710ed29e5057e0
sofiaa012 <158471277+sofiaa012@users.noreply.github.com>
2026-01-08 17:38:09 +0800

    Update chapter5.js

--- chapter5.js
+++ chapter5.js
@@ -1,18 +1,19 @@
 const MomentumLimit = 360; // 6 minutes (in seconds)
 
 const cardsData = [
-  { name: "laju", img: "images/ch5/c5Q1.png" },
-  { name: "laju", img: "images/ch5/c5A1.png" },
+  { name: "laju", img: "images/ch5/C5Q1.png" },
+  { name: "laju", img: "images/ch5/C5A1.png" },
 
-  { name: "gelombang", img: "images/ch5/c5Q2.png" },
-  { name: "gelombang", img: "images/ch5/c5A2.png" },
+  { name: "gelombang", img: "images/ch5/C5Q2.png" },
+  { name: "gelombang", img: "images/ch5/C5A2.png" },
 
-  { name: "lajugelombang", img: "images/ch5/c5Q3.png" },
-  { name: "lajugelombang", img: "images/ch5/c5A3.png" },
+  { name: "lajugelombang", img: "images/ch5/C5Q3.png" },
+  { name: "lajugelombang", img: "images/ch5/C5A3.png" },
 
-  { name: "pembiasan", img: "images/ch5/c5Q4.png" },
-  { name: "pembiasan", img: "images/ch5/c5A4.png" },
+  { name: "pembiasan", img: "images/ch5/C5Q4.png" },
+  { name: "pembiasan", img: "images/ch5/C5A4.png" },
 
-  { name: "panjang", img: "images/ch5/c5Q5.png" },
-  { name: "panjang", img: "images/ch5/c5A5.png" },
+  { name: "panjang", img: "images/ch5/C5Q5.png" },
+  { name: "panjang", img: "images/ch5/C5A5.png" },
 ];
+
