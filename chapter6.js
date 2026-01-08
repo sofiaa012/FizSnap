ac0b9fd22575d7890b18551f264ab3b82febaacc
sofiaa012 <158471277+sofiaa012@users.noreply.github.com>
2026-01-08 17:39:23 +0800

    Update chapter6.js

--- chapter6.js
+++ chapter6.js
@@ -1,18 +1,19 @@
 const MomentumLimit = 360; // 6 minutes (in seconds)
 
 const cardsData = [
-  { name: "indeks", img: "images/ch6/c6Q1.png" },
-  { name: "indeks", img: "images/ch6/c6A1.png" },
+  { name: "indeks", img: "images/ch6/C6Q1.png" },
+  { name: "indeks", img: "images/ch6/C6A1.png" },
 
-  { name: "hukum", img: "images/ch6/c6Q2.png" },
-  { name: "hukum", img: "images/ch6/c6A2.png" },
+  { name: "hukum", img: "images/ch6/C6Q2.png" },
+  { name: "hukum", img: "images/ch6/C6A2.png" },
 
-  { name: "formula", img: "images/ch6/c6Q3.png" },
-  { name: "formula", img: "images/ch6/c6A3.png" },
+  { name: "formula", img: "images/ch6/C6Q3.png" },
+  { name: "formula", img: "images/ch6/C6A3.png" },
 
-  { name: "cembung", img: "images/ch6/c6Q4.png" },
-  { name: "cembung", img: "images/ch6/c6A4.png" },
+  { name: "cembung", img: "images/ch6/C6Q4.png" },
+  { name: "cembung", img: "images/ch6/C6A4.png" },
 
-  { name: "cekung", img: "images/ch6/c6Q5.png" },
-  { name: "cekung", img: "images/ch6/c6A5.png" },
+  { name: "cekung", img: "images/ch6/C6Q5.png" },
+  { name: "cekung", img: "images/ch6/C6A5.png" },
 ];
+
