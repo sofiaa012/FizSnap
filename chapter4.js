8123770dafd66b9ab08b8bae940c1c574b871ac0
sofiaa012 <158471277+sofiaa012@users.noreply.github.com>
2026-01-08 14:31:16 +0800

    Add files via upload

---
+++ chapter4.js
@@ -0,0 +1,16 @@
+const MomentumLimit = 360; // 6 minutes (in seconds)
+
+const cardsData = [
+  { name: "muatan", img: "images/ch4/C4Q1.png" },
+  { name: "muatan", img: "images/ch4/C4A1.png" },
+
+  { name: "tentu", img: "images/ch4/C4Q2.png" },
+  { name: "tentu", img: "images/ch4/C4A2.png" },
+
+  { name: "pendam", img: "images/ch4/C4Q3.png" },
+  { name: "pendam", img: "images/ch4/C4A3.png" },
+
+  { name: "tekanan", img: "images/ch4/C4Q4.png" },
+  { name: "tekanan", img: "images/ch4/C4A4.png" },
+
+];
